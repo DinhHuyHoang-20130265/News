c5af290aea429d5561321ab524aa30205ebdf17a
DinhHuyHoang-20130265 <20130265@st.hcmuaf.eduvn>
2023-05-31 00:14:39 +0700

    finish fix bug Nguyen Si Hoang

--- app/src/main/java/com/example/news/dao/NewsDAO.java
+++ app/src/main/java/com/example/news/dao/NewsDAO.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.example.news.data.DbHelper;
 import com.example.news.models.Item;
@@ -16,7 +17,6 @@ import java.util.List;
 public class NewsDAO {
     private static final String TABLE_NAME = "News";
     private SQLiteDatabase db;
-    public static List<News> listNews = new ArrayList<>();
 
     public NewsDAO(Context context) {
         DbHelper dbHelper = new DbHelper(context);
@@ -30,9 +30,12 @@ public class NewsDAO {
         values.put("date", item.getDate());
         values.put("linkImg", item.getLinkImg());
         try {
-            if (db.insert("Saved", null, values) != -1) ;
+            db.insert("Saved", null, values);
+            Log.d("success_Insert", "Success");
+            Log.d("Table_after", getSaved().toString());
             return true;
         } catch (Exception e) {
+            Log.d("fail_Insert", e.toString());
             return false;
         }
     }
@@ -42,7 +45,7 @@ public class NewsDAO {
         values.put("name", news.getName());
         values.put("link", news.getLink());
         try {
-            if (db.insert(TABLE_NAME, null, values) != -1) ;
+            db.insert(TABLE_NAME, null, values);
             return true;
         } catch (Exception e) {
             return false;
@@ -56,7 +59,7 @@ public class NewsDAO {
         values.put("link", news.getLink());
         String strID = String.valueOf(news.getId());
         try {
-            if (db.update(TABLE_NAME, values, "id = ?", new String[]{strID}) != -1) ;
+            db.update(TABLE_NAME, values, "id = ?", new String[]{strID});
             return true;
         } catch (Exception e) {
             return false;
@@ -65,10 +68,7 @@ public class NewsDAO {
 
     public boolean delete(int id) {
         String strID = String.valueOf(id);
-        if (db.delete(TABLE_NAME, "id = ?", new String[]{strID}) >= 0) {
-            return true;
-        }
-        return false;
+        return db.delete(TABLE_NAME, "id = ?", new String[]{strID}) >= 0;
     }
 
     public List<News> getALL() {
@@ -79,16 +79,13 @@ public class NewsDAO {
     public News getNews(int id) {
         String sql = "SELECT * FROM " + TABLE_NAME + " WHERE id=?";
         List<News> list = getData(sql, String.valueOf(id));
-        if (list == null) {
-            return null;
-        }
         return list.get(0);
     }
 
     @SuppressLint("Range")
     private List<News> getData(String sql, String... selectionArgs) {
         List<News> list = new ArrayList<>();
-        Cursor cursor = db.rawQuery(sql, selectionArgs);
+        @SuppressLint("Recycle") Cursor cursor = db.rawQuery(sql, selectionArgs);
         while (cursor.moveToNext()) {
             News obj = new News();
             obj.setId(cursor.getInt(cursor.getColumnIndex("id")));
@@ -102,12 +99,13 @@ public class NewsDAO {
     @SuppressLint("Range")
     private List<Item> getDataSaved(String sql, String... selectionArgs) {
         List<Item> list = new ArrayList<>();
-        Cursor cursor = db.rawQuery(sql, selectionArgs);
+        @SuppressLint("Recycle") Cursor cursor = db.rawQuery(sql, selectionArgs);
         while (cursor.moveToNext()) {
             Item obj = new Item();
+            obj.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex("id"))));
             obj.setTitle(cursor.getString(cursor.getColumnIndex("title")));
             obj.setLink(cursor.getString(cursor.getColumnIndex("link")));
-            obj.setDate(cursor.getString(cursor.getColumnIndex("dPost")));
+            obj.setDate(cursor.getString(cursor.getColumnIndex("date")));
             obj.setLinkImg(cursor.getString(cursor.getColumnIndex("linkImg")));
             list.add(obj);
         }
@@ -117,5 +115,10 @@ public class NewsDAO {
     public List<Item> getSaved() {
         String sql = "SELECT * FROM " + "Saved";
         return getDataSaved(sql);
+    }
+
+    public boolean deleteSaved(Item item) {
+        String strID = String.valueOf(item.getId());
+        return db.delete("Saved", "id = ?", new String[]{strID}) >= 0;
     }
 }
--- app/src/main/java/com/example/news/xmlpullparser/XmlPullParserHandler.java
+++ app/src/main/java/com/example/news/xmlpullparser/XmlPullParserHandler.java
@@ -15,23 +15,25 @@ import java.util.List;
 
 public class XmlPullParserHandler {
     public static final String ns = null;
-    public List<Item> Pasers(InputStream in) throws XmlPullParserException , IOException {
+
+    public List<Item> Pasers(InputStream in) throws XmlPullParserException, IOException {
         try {
             XmlPullParser parser = Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(in, null);
             parser.nextTag();
             return readRss(parser);
-        }finally {
+        } finally {
             in.close();
         }
     }
-    private List<Item> readRss(XmlPullParser parser) throws XmlPullParserException , IOException {
+
+    private List<Item> readRss(XmlPullParser parser) throws XmlPullParserException, IOException {
         Log.i("-----", "read rss");
-        List<Item> list = new ArrayList<Item>();
+        List<Item> list = new ArrayList<>();
         parser.require(XmlPullParser.START_TAG, ns, "rss");
-        while (parser.next() != XmlPullParser.END_TAG){
-            if (parser.getEventType() != XmlPullParser.START_TAG){
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
             }
             String name1 = parser.getName();
@@ -42,9 +44,9 @@ public class XmlPullParserHandler {
                         continue;
                     }
                     String name = parser.getName();
-                    if (name.equals("item")){
+                    if (name.equals("item")) {
                         list.add(readItem(parser));
-                    }else {
+                    } else {
                         skip(parser);
                     }
                 }
@@ -54,32 +56,38 @@ public class XmlPullParserHandler {
     }
 
 
-    private Item readItem(XmlPullParser parser) throws XmlPullParserException , IOException {
+    private Item readItem(XmlPullParser parser) throws XmlPullParserException, IOException {
         Log.i("-----", "read Item");
         parser.require(XmlPullParser.START_TAG, ns, "item");
         String title = null;
         String link = null;
         String pubDate = null;
         String img = null;
-        while (parser.next() != XmlPullParser.END_TAG){
-            if (parser.getEventType() != XmlPullParser.START_TAG){
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
                 continue;
             }
             String name = parser.getName();
-            if (name.equals("title")){
-                title = readTitle(parser);
-                Log.i("-------title ",title);
-            }else if (name.equals("link")){
-                link = readLink(parser);
-                Log.i("-------link ",link);
-            }else if (name.equals("description")){
-                img = readImgLink(parser);
-                Log.i("-------link img",img);
-            }else if (name.equals("pubDate")){
-                pubDate = readDate(parser);
-                Log.i("-------pubDate ",pubDate);
-            }else {
-                skip(parser);
+            switch (name) {
+                case "title":
+                    title = readTitle(parser);
+                    Log.i("-------title ", title);
+                    break;
+                case "link":
+                    link = readLink(parser);
+                    Log.i("-------link ", link);
+                    break;
+                case "description":
+                    img = readImgLink(parser);
+                    Log.i("-------link img", img);
+                    break;
+                case "pubDate":
+                    pubDate = readDate(parser);
+                    Log.i("-------pubDate ", pubDate);
+                    break;
+                default:
+                    skip(parser);
+                    break;
             }
 
         }
@@ -87,35 +95,35 @@ public class XmlPullParserHandler {
     }
 
 
-    private String getLinkFromText(String a){
-        String link = "";
-        if (a.contains("img")){
+    private String getLinkFromText(String a) {
+        if (a.contains("img")) {
             int posImg = a.indexOf("img");// lấy vị trí chuỗi "img" trong chỗi
             int posDong = a.indexOf("/>");
             a = a.substring(posImg, posDong);
-            a = a.substring(a.indexOf("\"")+1, a.length() - 2);
+            a = a.substring(a.indexOf("\"") + 1, a.length() - 2);
         }
         return a;
     }
-    private String readImgLink(XmlPullParser parser)throws XmlPullParserException , IOException{
-        Log.i("-----", "read description"+parser.getText());
+
+    private String readImgLink(XmlPullParser parser) throws XmlPullParserException, IOException {
+        Log.i("-----", "read description" + parser.getText());
         String imageLink = "";
         parser.require(XmlPullParser.START_TAG, ns, "description");
-       while (parser.next() != XmlPullParser.END_TAG){
-           String name = parser.getText();
-           imageLink = getLinkFromText(name);
-           Log.i("-----", "read description IMG SRC: "+imageLink);
-       }
+        while (parser.next() != XmlPullParser.END_TAG) {
+            String name = parser.getText();
+            imageLink = getLinkFromText(name);
+            Log.i("-----", "read description IMG SRC: " + imageLink);
+        }
         return imageLink;
     }
 
-    private void skip(XmlPullParser parser) throws XmlPullParserException , IOException {
-        if (parser.getEventType() != XmlPullParser.START_TAG){
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
             throw new IllegalStateException();
         }
         int depth = 1;
-        while (depth != 0){
-            switch (parser.next()){
+        while (depth != 0) {
+            switch (parser.next()) {
                 case XmlPullParser.END_TAG:
                     depth--;
                     break;
@@ -125,30 +133,34 @@ public class XmlPullParserHandler {
             }
         }
     }
-    private String readText(XmlPullParser parser) throws XmlPullParserException , IOException {
+
+    private String readText(XmlPullParser parser) throws XmlPullParserException, IOException {
         //Log.i("-----", "read text");
         String rs = "";
-        if (parser.next() == XmlPullParser.TEXT){
+        if (parser.next() == XmlPullParser.TEXT) {
             rs = parser.getText();
             parser.nextTag();
         }
         return rs;
     }
-    private String readTitle(XmlPullParser parser) throws XmlPullParserException , IOException {
+
+    private String readTitle(XmlPullParser parser) throws XmlPullParserException, IOException {
         Log.i("-----", "read title");
         parser.require(XmlPullParser.START_TAG, ns, "title");
         String title = readText(parser);
         parser.require(XmlPullParser.END_TAG, ns, "title");
         return title;
     }
-    private String readLink(XmlPullParser parser) throws XmlPullParserException , IOException {
+
+    private String readLink(XmlPullParser parser) throws XmlPullParserException, IOException {
         Log.i("-----", "read link");
         parser.require(XmlPullParser.START_TAG, ns, "link");
         String link = readText(parser);
         parser.require(XmlPullParser.END_TAG, ns, "link");
         return link;
     }
-    private String readDate(XmlPullParser parser) throws XmlPullParserException , IOException {
+
+    private String readDate(XmlPullParser parser) throws XmlPullParserException, IOException {
         Log.i("-----", "read date");
         parser.require(XmlPullParser.START_TAG, ns, "pubDate");
         String pubDate = readText(parser);
