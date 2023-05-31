5a9ae88803676f37226a9a484739602d19b2e71d
DinhHuyHoang-20130265 <20130265@st.hcmuaf.eduvn>
2023-05-31 14:24:27 +0700

    finish contentView

--- app/src/main/java/com/example/news/HistoryActivity.java
+++ app/src/main/java/com/example/news/HistoryActivity.java
@@ -1,23 +1,35 @@
 package com.example.news;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.news.dao.NewsDAO;
 import com.example.news.models.Item;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class HistoryActivity extends AppCompatActivity {
     ListView lv;
@@ -30,15 +42,21 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         lv = findViewById(R.id.lv_history);
-        Intent intent = getIntent();
 
         lv.setOnItemClickListener((adapterView, view, i, l) -> {
-
+            openLink(i);
         });
         lv.setOnItemLongClickListener((AdapterView.OnItemLongClickListener) (adapterView, view, i, l) -> {
-            openDialog(ItemLists.get(i));
+            openDialog(i);
             return false;
         });
+    }
+
+    public void openLink(int i) {
+        Toast.makeText(getApplicationContext(), "click", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(HistoryActivity.this, WebViewActivity.class);
+        intent.putExtra("linknews", ItemLists.get(i).getLink());
+        startActivity(intent);
     }
 
     public void initHistory() {
@@ -48,10 +66,12 @@ public class HistoryActivity extends AppCompatActivity {
             List<Item> items = new ArrayList<>();
             editor.putString("history", new Gson().toJson(items));
             editor.apply();
+        } else {
+
         }
     }
 
-    public void openDialog(Item item) {
+    public void openDialog(int i) {
         dialog = new Dialog(HistoryActivity.this);
         dialog.setContentView(R.layout.dialog_add);
         btn_add = dialog.findViewById(R.id.btn_add);
@@ -60,5 +80,53 @@ public class HistoryActivity extends AppCompatActivity {
 
         });
         dialog.show();
+    }
+
+    public void UpdateLV() {
+        ArrayList<Item> list = (ArrayList<Item>) new NewsDAO(HistoryActivity.this).getSaved();
+        if (!list.isEmpty()) {
+            TextView empty = findViewById(R.id.empty);
+            empty.setVisibility(View.INVISIBLE);
+            HistoryAdapter adapter = new HistoryAdapter(getApplicationContext(), HistoryActivity.this, list);
+            lv.setAdapter(adapter);
+        }
+    }
+
+    public class HistoryAdapter extends ArrayAdapter<Item> {
+        private final Context context;
+        private final ArrayList<Item> list;
+        private final HistoryActivity main;
+
+        public HistoryAdapter(@NonNull Context context, HistoryActivity activity, ArrayList<Item> list) {
+            super(context, 0, list);
+            this.context = context;
+            this.list = list;
+            this.main = activity;
+        }
+
+        @SuppressLint("InflateParams")
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = inflater.inflate(R.layout.item_newss, null);
+            }
+            final Item item = list.get(position);
+            if (v != null) {
+                ImageView iv_news = v.findViewById(R.id.iv_news);
+                TextView tv_title_item_news = v.findViewById(R.id.tv_title_item_news);
+                TextView tv_link_news = v.findViewById(R.id.tv_link_news);
+                TextView tv_date_news = v.findViewById(R.id.tv_date_news);
+                Picasso.with(getContext()).load(item.getLinkImg()).into(iv_news);
+                tv_title_item_news.setText(item.getTitle());
+                tv_link_news.setText(item.getLink());
+                tv_date_news.setText(item.getDate());
+            }
+
+            assert v != null;
+            return v;
+        }
     }
 }
--- app/src/main/java/com/example/news/WebViewActivity.java
+++ app/src/main/java/com/example/news/WebViewActivity.java
@@ -1,23 +1,99 @@
 package com.example.news;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
-public class WebViewActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    WebView webView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class WebViewActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    private ImageView mImgBackHomeRight;
+    private WebSettings webSettings = null;
+    private WebView webView;
+    private String link;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-        webView = findViewById(R.id.webview);
         Intent intent = getIntent();
-        String link = intent.getStringExtra("linknews");
-        webView.loadUrl(link);
+        link = intent.getStringExtra("linknews");
+        initView();
+
+        webView.setWebChromeClient(new WebChromeClient());
+        MyAsyncTask myAsynTask = new MyAsyncTask(link);
+        myAsynTask.execute();
+    }
+
+    private void initView() {
+        mImgBackHomeRight = findViewById(R.id.img_back_right);
+        mImgBackHomeRight.setOnClickListener(this);
+        webView = findViewById(R.id.wv_news);
         webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+        configWebView();
+    }
+
+    private void configWebView() {
+        webSettings.setRenderPriority(WebSettings.RenderPriority.LOW );
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setAppCacheEnabled(false);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setGeolocationEnabled(false);
+        webSettings.setNeedInitialFocus(false);
+        webSettings.setSaveFormData(false);
+    }
+
+    private class MyAsyncTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+
+        public MyAsyncTask(String url) {
+            this.url = url;
+        }
+
+        @Override
+        protected String doInBackground(Void... s) {
+
+            Document document;
+
+            String content = null;
+            try {
+                document = Jsoup.connect(url).get();
+                Elements element = document.getElementsByClass("noidung");
+                content = element.get(0).toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String document) {
+            super.onPostExecute(document);
+            webView.loadDataWithBaseURL(link, document, "text/html", "utf-8", "");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.img_back_right) {
+            super.onBackPressed();
+        }
     }
 }
